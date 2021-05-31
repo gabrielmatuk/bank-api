10afdcabd5ac8cc590979b5dddf8e4a47ac9658d
Gabriel Matuk <gabriel.matuk@eteg.com.br>
2021-05-31 20:30:05 -0300

    feature/installing winston, changing log message

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 import express from "express";
+import winston from "winston";
 import accountsRouter from "./routes/accounts.js"
 import {promises as fs} from "fs"
 const {readFile, writeFile} = fs
 
 global.fileName = "accounts.json"
+
+const { combine, timestamp, label, printf} = winston.format;
+const myFormat = printf(({level, message, label, timestamp})=> {
+    return `${timestamp}, [${label}] ${level} ${message}`
+})
+global.logger = winston.createLogger({
+    level:"silly", 
+    transports: [
+        new(winston.transports.Console),
+        new (winston.transports.File)({filename: "my-bank-api.log"})
+    ],
+    format: combine(
+        label({ label: "my-bank-api"}),
+        timestamp(),
+        myFormat
+    )
+});
+
 const app = express();
 app.use(express.json());
 
@@ -13,16 +32,16 @@ app.listen(3000, async () => {
 
     try{
         await readFile(global.fileName)
-        console.log("API Started on port 3000 ")
+        logger.info("API Started on port 3000 ")
     }catch(err){
         const initialJson = {
             nextId: 1,
             accounts: []    
         }
         writeFile(global.fileName, JSON.stringify(initialJson)).then(()=>{
-            console.log("API Started on port 3000 ")
+            logger.info("API Started on port 3000 ")
         }).catch(err =>{
-            console.log(err);
+            logger.error(err);
         });
     }
  
--- routes/accounts.js
+++ routes/accounts.js
@@ -1,10 +1,10 @@
 import express from "express";
 const router = express.Router();
-import {promises as fs} from "fs"
+import {promises as fs} from "fs";
 
 const {readFile, writeFile} = fs
 
-router.post("/", async (req,res)=> {
+router.post("/", async (req,res, next)=> {
     try{
         let account = (req.body);
         const data = JSON.parse(await readFile(global.fileName));
@@ -15,33 +15,37 @@ router.post("/", async (req,res)=> {
         await writeFile(global.fileName, JSON.stringify(data, null, 2));
 
         res.send(account);
+
+        logger.info(`"POST /account - ${JSON.stringify(account)}"`)
     }catch(err){
-        res.status(400).send({error: err.message})
+        next(err);
     }
     
 });
 
-router.get("/", async(_,res)=>{
+router.get("/", async(_,res, next)=>{
     try{
         const data = JSON.parse(await readFile(global.fileName));
         delete data.nextId;
         res.send(data);
+        logger.info("GET /account")
     }catch(err){
-        res.status(400).send({error: err.message});
+        next(err)
     };
 });
 
-router.get("/:id", async(req,res)=> {
+router.get("/:id", async(req,res,next)=> {
     try{
         const data = JSON.parse(await readFile(global.fileName));
         const account = data.accounts.find(account => account.id === parseInt(req.params.id));
         res.send(account);
+        logger.ifno("GET /account/:id")
     }catch(err){
-        res.status(400).send({error: err.message});
+        next(err);
     }
 });
 
-router.delete("/:id", async(req,res) => {
+router.delete("/:id", async(req,res, next) => {
     try{
         const data = JSON.parse(await readFile(global.fileName));
         data.accounts = data.accounts.filter(account => account.id !== parseInt(req.params.id));
@@ -49,14 +53,14 @@ router.delete("/:id", async(req,res) => {
         await writeFile(global.fileName, JSON.stringify(data,null,2));
 
         res.end();
+        logger.info(`DELETE /account/:id - ${req.params.id}`)
     }catch(err){
-        res.status(400).send({error: err.message});
-
+        next(err);
     }
 
 });
 
-router.put("/", async (req, res)=> {
+router.put("/", async (req, res,next)=> {
     try{
         const account = req.body;
 
@@ -68,12 +72,13 @@ router.put("/", async (req, res)=> {
         await writeFile(global.fileName, JSON.stringify(data));
 
         res.send(account);
+        logger.info(`PUT /acccount - ${JSON.stringify(account)}`)
     }catch(err){
-        res.status(400).send({error: err.message});
+        next(err);
     }
 });
 
-router.patch("/updateBalance", async(req,res)=> {
+router.patch("/updateBalance", async(req,res, next)=> {
     try{
         const account = req.body;
 
@@ -85,9 +90,15 @@ router.patch("/updateBalance", async(req,res)=> {
         await writeFile(global.fileName, JSON.stringify(data));
 
         res.send(data.accounts[index]);
+        logger.info(`PATCH /acccount/updateBalance - ${JSON.stringify(account)}`)
     }catch(err){
-        res.status(400).send({error: err.message});
+       next(err)
     }
+});
+
+router.use((err, req, res, next)=> {
+    logger.error(`${req.method} ${req.baseUrl} - ${err.message}`)
+    res.status(400).send({error: err.message});
 });
 
 export default router;
