c09c706e6d211b88666e12daeddafe66e0a3f291
Gabriel Matuk <gabriel.matuk@eteg.com.br>
2021-05-31 19:09:31 -0300

    feature/insert route GET for account info and setting Global variable

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import accountsRouter from "./routes/accounts.js"
 import {promises as fs} from "fs"
 const {readFile, writeFile} = fs
 
+global.fileName = "accounts.json"
 const app = express();
 app.use(express.json());
 
@@ -11,14 +12,14 @@ app.use("/account", accountsRouter);
 app.listen(3000, async () => {
 
     try{
-        await readFile("accounts.json")
+        await readFile(global.fileName)
         console.log("API Started on port 3000 ")
     }catch(err){
         const initialJson = {
             nextId: 1,
             accounts: []    
         }
-        writeFile("accounts.json", JSON.stringify(initialJson)).then(()=>{
+        writeFile(global.fileName, JSON.stringify(initialJson)).then(()=>{
             console.log("API Started on port 3000 ")
         }).catch(err =>{
             console.log(err);
--- routes/accounts.js
+++ routes/accounts.js
@@ -7,12 +7,12 @@ const {readFile, writeFile} = fs
 router.post("/", async (req,res)=> {
     try{
         let account = (req.body);
-        const data = JSON.parse(await readFile("accounts.json"));
+        const data = JSON.parse(await readFile(global.fileName));
 
         account = {id: data.nextId++, ...account};
         data.accounts.push(account);
 
-        await writeFile("accounts.json", JSON.stringify(data, null, 2));
+        await writeFile(global.fileName, JSON.stringify(data, null, 2));
 
         res.send(account);
     }catch(err){
@@ -20,5 +20,16 @@ router.post("/", async (req,res)=> {
     }
     
 });
+
+router.get("/", async(req,res)=>{
+    try{
+        const data = JSON.parse(await readFile(global.fileName));
+        delete data.nextId;
+        res.send(data);
+    }catch(err){
+        res.status(400).send({error: err.message});
+    };
+});
+
 
 export default router;
