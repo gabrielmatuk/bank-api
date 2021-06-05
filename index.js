30f3d69823cbff186ac199d89856c60f3af37430
Gabriel Matuk <gabriel.matuk@eteg.com.br>
2021-06-05 14:43:01 -0300

    add cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import winston from "winston";
 import accountsRouter from "./routes/accounts.js"
 import {promises as fs} from "fs"
+import cors from "cors";
 const {readFile, writeFile} = fs
 
 global.fileName = "accounts.json"
@@ -25,7 +26,8 @@ global.logger = winston.createLogger({
 
 const app = express();
 app.use(express.json());
-
+app.use(express.static("public"));
+app.use(cors());
 app.use("/account", accountsRouter);
 
 app.listen(3000, async () => {
